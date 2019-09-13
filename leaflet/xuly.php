18cb973a42a86f7ab997c71f4b91dbb62f17a14b
soiqualang <soiqualang.chentreu@gmail.com>
2019-09-13 17:31:45 +0700

    wms get geojson

--- leaflet/xuly.php
+++ leaflet/xuly.php
@@ -1,22 +1,14 @@
-# vinagit_class
-Vinagit class
+<?php
+include('db.php');
 
-# Leaflet
-
-https://leafletjs.com/
-
-https://dothanhlong.org/OpenLayers3-Leaflte-Cookbook/leaflet/README.html
-
-
-```sql
-select row_to_json(fc)
+$sql='select row_to_json(fc)
 from (
     select
-        'FeatureCollection' as "type",
+        \'FeatureCollection\' as "type",
         array_to_json(array_agg(f)) as "features"
     from (
         select
-            'Feature' as "type",
+            \'Feature\' as "type",
             ST_AsGeoJSON(ST_Transform(geom, 4326), 6) :: json as "geometry",
             (
                 select json_strip_nulls(row_to_json(t))
@@ -30,8 +22,11 @@ from (
             id=3880
         limit 10
     ) as f
-) as fc;
-```
+) as fc;';
 
+$query=pg_query($dbcon,$sql);
 
-https://www.postgis.net/docs/ST_AsGeoJSON.html
+while($kq=pg_fetch_assoc($query)){
+	echo $kq['row_to_json'];
+}
+?>
