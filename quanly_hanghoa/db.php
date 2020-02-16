1204ecd0c5d8c2f143571983a85bf8ce2bfedc11
soiqualang <soiqualang.chentreu@gmail.com>
2020-02-16 19:04:44 +0700

    Update db.php

--- quanly_hanghoa/db.php
+++ quanly_hanghoa/db.php
@@ -2,7 +2,7 @@
     define("PG_DB","quanly_hanghoa");
     define("PG_HOST","localhost");
     define("PG_USER","postgres");
-    define("PG_PORT","5433");
-    define("PG_PASS","2679191");
+    define("PG_PORT","5432");
+    define("PG_PASS","**************");
     $dbcon=pg_connect("dbname=".PG_DB." user=".PG_USER." password=".PG_PASS." host=".PG_HOST." port=".PG_PORT);
 ?>
